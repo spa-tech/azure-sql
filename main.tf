9d36bf1080b50d8be45aae8d18293b940f359f90
Jason Spangler <9028535+jaspangler@users.noreply.github.com>
2023-02-01 14:17:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "azurerm_mssql_database" "emaildb" {
   server_id      = azurerm_mssql_server.server1.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  sku_name       = "S0"
+  sku_name       = "Basic"
   zone_redundant = false
 
   tags = {
