5ffe8fb97692567cfbabd2b86bb7ed337bfd94e5
Jason Spangler <9028535+jaspangler@users.noreply.github.com>
2023-02-03 10:12:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,8 +35,9 @@ resource "azurerm_mssql_server" "server1" {
   }
 }
 resource "azurerm_mssql_database" "emaildb" {
-  name           = "emaildb"
-  server_id      = azurerm_mssql_server.server1.id
-  sku_name       = "GP_S_Gen5_1"
-  min_capacity   = "0.5"
+  name                        = "emaildb"
+  server_id                   = azurerm_mssql_server.server1.id
+  sku_name                    = "GP_S_Gen5_1"
+  min_capacity                = "0.5"
+  auto_pause_delay_in_minutes = "60"
 }
