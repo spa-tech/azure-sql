20e4f646ca07a0336a26607056a862c96adfae33
Jason Spangler <9028535+jaspangler@users.noreply.github.com>
2023-02-03 13:04:13 -0500

    Update and rename terraform.tf to vars.tf

--- terraform.tf
+++
@@ -1,7 +0,0 @@
-variable "azuread_admin_login_username" {
- type = string
-}
-
-variable "azuread_admin_login_object_id" {
- type = string
-}
---
+++ vars.tf
@@ -0,0 +1,27 @@
+variable "azuread_admin_login_username" {
+ type = string
+}
+
+variable "azuread_admin_login_object_id" {
+ type = string
+}
+
+variable "admin_login_username" {
+ type = string
+}
+
+variable "admin_login_password" {
+ type = string
+}
+
+variable "mssql_server_name" {
+ type = string
+}
+
+variable "mssql_firewall_rule_start_ip" {
+ type = string
+}
+
+variable "mssql_firewall_rule_end_ip" {
+ type = string
+}
